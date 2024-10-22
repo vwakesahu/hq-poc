3adbbfc3dd53667bbc3836b648c8b9d2d68de093
Vivek Sahu <viveksahu1762@gmail.com>
2024-10-22 18:12:04 +0530

    added: claim tokens

--- src/app/transfer/page.js
+++ src/app/transfer/page.js
@@ -52,56 +52,6 @@ const TransferForm = () => {
     return address;
   };
 
-  const getBalance = async () => {
-    setBalanceLoading(true);
-    // setError(null);
-    try {
-      const { publicKey, privateKey } = fhevmInstance.generateKeypair();
-      const eip712 = fhevmInstance.createEIP712(
-        publicKey,
-        ENCRYPTEDERC20CONTRACTADDRESS
-      );
-
-      const signature = await signer._signTypedData(
-        eip712.domain,
-        { Reencrypt: eip712.types.Reencrypt },
-        eip712.message
-      );
-      const encryptedErc20Contract = new Contract(
-        ENCRYPTEDERC20CONTRACTADDRESS,
-        ENCRYPTEDERC20CONTRACTABI,
-        signer
-      );
-      const balanceHandle = await encryptedErc20Contract.balanceOf(address);
-      console.log("balanceHandle", balanceHandle);
-      if (balanceHandle.toString() === "0") {
-        console.log("You have no balance to fetch");
-        // toast.error("You have no balance to fetch.");
-        setBalance(null);
-      } else {
-        const balanceResult = await fhevmInstance.reencrypt(
-          balanceHandle,
-          privateKey,
-          publicKey,
-          signature.replace("0x", ""),
-          ENCRYPTEDERC20CONTRACTADDRESS,
-          address
-        );
-        console.log(balanceResult);
-        setBalance(balanceResult.toString());
-      }
-    } catch (err) {
-      console.error(err);
-      setBalance(null);
-      // toast.error("Failed to fetch balance. Please try again.");
-      // setError("Failed to fetch balance. Please try again.");
-      // setIsErrorModalOpen(true);
-    } finally {
-      console.log("completed");
-      setBalanceLoading(false);
-    }
-  };
-
   const reviewPayments = async () => {
     console.log("Form values:", payments);
 
@@ -156,77 +106,15 @@ const TransferForm = () => {
     }
   };
 
-  const getUSDCBalance = async () => {
-    try {
-      setUsdcBalanceLoading(true);
-      const erc20Contract = new Contract(
-        ERC20CONTRACTADDRESS,
-        ERC20CONTRACTABI,
-        signer
-      );
-      const balance = await erc20Contract.balanceOf(address);
-      console.log(balance.toString());
-      setUsdcBalance(balance.toString());
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setUsdcBalanceLoading(false);
-    }
-  };
-
   return (
-    <div className="max-w-4xl mx-auto pb-8 flex flex-col h-full">
+    <div className="max-w-4xl mx-auto  flex flex-col h-full">
       <Card className="">
         <CardContent className="p-6">
           <div className="flex w-full justify-between">
             <h2 className="text-lg font-semibold pb-4">
-              Payments ({payments.length})
+              Confidential Transfers
             </h2>
-            <div className="flex gap-2 items-center">
-              <Button
-                className="flex gap-2"
-                variant="outline"
-                onClick={getUSDCBalance}
-                disabled={usdcBalanceLoading}
-              >
-                {usdcBalanceLoading ? (
-                  <Loader className="animate-spin" />
-                ) : (
-                  <>
-                    {usdcBalance ? (
-                      `USDC Balance: ${usdcBalance}`
-                    ) : (
-                      <>
-                        <RefreshCcw size={16} />
-                        USDC
-                      </>
-                    )}
-                  </>
-                )}
-              </Button>
-              <Button
-                className="flex gap-2"
-                variant="outline"
-                onClick={getBalance}
-                disabled={balanceLoading}
-              >
-                {balanceLoading ? (
-                  <Loader className="animate-spin" />
-                ) : (
-                  <>
-                    {balance ? (
-                      `cUSDC Balance: ${balance}`
-                    ) : (
-                      <>
-                        <RefreshCcw size={16} />
-                        CUSDC
-                      </>
-                    )}
-                  </>
-                )}
-                {/* nce */}
-              </Button>
-            </div>
+
             {/* <Button
               onClick={addPayment}
               className="bg-gray-900 hover:bg-gray-700 text-white"
@@ -313,11 +201,198 @@ const TransferForm = () => {
               <Plus size={16} />
             </Button>
             <Button
-          className="bg-gray-900 hover:bg-gray-700 text-white px-6"
-          onClick={reviewPayments}
-        >
-          Review Payments
-        </Button>
+              className="bg-gray-900 hover:bg-gray-700 text-white px-6"
+              onClick={reviewPayments}
+            >
+              Review Payments
+            </Button>
+          </div>
+
+          {/* <Button
+            variant="secondary"
+            onClick={addPayment}
+            className="flex items-center border bg-white hover:bg-white/70"
+          >
+            <Plus size={16} className="mr-2" />
+            Add another payment
+          </Button> */}
+        </CardContent>
+      </Card>
+
+      <div className="mt-8 flex justify-end gap-4"></div>
+    </div>
+  );
+};
+
+const WrapAndDistribute = () => {
+  const { signer, w0, address, isLoading, error } = useWalletContext();
+  const [payments, setPayments] = useState([
+    {
+      recipient: "",
+      amount: "",
+    },
+  ]);
+  const { instance: fhevmInstance } = useFhevm();
+  const [balance, setBalance] = useState(null);
+  const [balanceLoading, setBalanceLoading] = useState(false);
+  const [usdcBalanceLoading, setUsdcBalanceLoading] = useState(false);
+  const [usdcBalance, setUsdcBalance] = useState(null);
+
+  const addPayment = () => {
+    setPayments([...payments, { recipient: "", amount: "" }]);
+  };
+
+  const removePayment = (index) => {
+    setPayments(payments.filter((_, i) => i !== index));
+  };
+
+  const updatePayment = (index, field, value) => {
+    const updatedPayments = [...payments];
+    updatedPayments[index][field] = value;
+    setPayments(updatedPayments);
+  };
+
+  const reviewPayments = async () => {
+    console.log("Form values:", payments);
+
+    const encryptedERC20Contract = new Contract(
+      ENCRYPTEDERC20CONTRACTADDRESS,
+      ENCRYPTEDERC20CONTRACTABI,
+      signer
+    );
+
+    for (let i = 0; i < payments.length; i++) {
+      try {
+        const input = await fhevmInstance.createEncryptedInput(
+          ENCRYPTEDERC20CONTRACTADDRESS,
+          address
+        );
+        input.add64(Number(payments[i].amount));
+        const encryptedInput = input.encrypt();
+        // console.log(
+        //   "Encryted input",
+        //   await toHexString(encryptedInput.inputProof)
+        // );
+        // console.log(
+        //   "Encryted input",
+        //   await toHexString(encryptedInput.inputProof)
+        // );
+
+        const response = await encryptedERC20Contract[
+          "transfer(address,bytes32,bytes)"
+        ](
+          payments[i].recipient,
+          encryptedInput.handles[0],
+          "0x" + toHexString(encryptedInput.inputProof)
+        );
+
+        const tx = await response.getTransaction();
+        console.log(tx);
+        const receipt = await tx.wait();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  return (
+    <div className="max-w-4xl mx-auto flex flex-col h-full">
+      <Card className="">
+        <CardContent className="p-6">
+          <div className="flex w-full justify-between">
+            <h2 className="text-lg font-semibold pb-4">Wrap & Distribute</h2>
+
+            {/* <Button
+              onClick={addPayment}
+              className="bg-gray-900 hover:bg-gray-700 text-white"
+            >
+              <Plus size={16} />
+            </Button> */}
+          </div>
+
+          <div className="max-h-80 overflow-y-auto mb-4 py-1 scrollbar-hidden">
+            <div className="space-y-4">
+              {payments.map((payment, index) => (
+                <Card
+                  key={index}
+                  className="border border-black/15 shadow-none bg-muted"
+                >
+                  <CardContent className="p-4">
+                    <div className="flex items-center justify-between mb-4">
+                      <span className="font-medium text-gray-700">
+                        Payment {index + 1}
+                      </span>
+                      {index > 0 && (
+                        <Button
+                          variant="ghost"
+                          size="sm"
+                          onClick={() => removePayment(index)}
+                          className="text-red-500 hover:text-red-700 hover:border hover:border-red-700 hover:bg-red-50"
+                        >
+                          <Trash2 size={16} />
+                        </Button>
+                      )}
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                      <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                          Recipient *
+                        </label>
+                        <div className="relative">
+                          <Input
+                            placeholder="Enter recipient address"
+                            className="w-full pl-10 bg-white"
+                            value={payment.recipient}
+                            onChange={(e) =>
+                              updatePayment(index, "recipient", e.target.value)
+                            }
+                          />
+                          <WalletIcon
+                            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+                            size={18}
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
+                          Amount *
+                        </label>
+                        <div className="relative">
+                          <Input
+                            placeholder="Enter amount"
+                            className="w-full pl-10 bg-white"
+                            type="number"
+                            value={payment.amount}
+                            onChange={(e) =>
+                              updatePayment(index, "amount", e.target.value)
+                            }
+                          />
+                          <CreditCard
+                            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+                            size={18}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </div>
+          <div className="flex items-center justify-end w-full gap-2">
+            <Button
+              onClick={addPayment}
+              variant="ghost"
+              // className="bg-gray-900 hover:bg-gray-700 text-white"
+            >
+              <Plus size={16} />
+            </Button>
+            <Button
+              className="bg-gray-900 hover:bg-gray-700 text-white px-6"
+              onClick={reviewPayments}
+            >
+              Wrap & Distribute
+            </Button>
           </div>
 
           {/* <Button
@@ -339,18 +414,13 @@ const TransferForm = () => {
 const Page = () => {
   return (
     <BasicPageLayout title="Confidential Transfers">
-      <TransferForm />
+      <WrapAndDistribute />
+
+      <div>
+        <TransferForm />
+      </div>
     </BasicPageLayout>
   );
 };
 
 export default Page;
-
-const RefreshBalanceButton = () => {
-  return (
-    <div className="flex gap-2" variant="outline">
-      <RefreshCcw size={16} className="animate-spin" />
-      {/* <span className="font-semibold">14000</span> */}
-    </div>
-  );
-};
--- src/layout/basic-page-layout.js
+++ src/layout/basic-page-layout.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useFhevm } from "@/fhevm/fhevm-context";
 import { useWalletContext } from "@/privy/walletContext";
 import {
   ENCRYPTEDERC20CONTRACTABI,
@@ -9,12 +10,17 @@ import {
   ERC20CONTRACTADDRESS,
 } from "@/utils/contracts";
 import { Contract } from "ethers";
-import { Loader } from "lucide-react";
+import { Loader, RefreshCcw } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
 
 const BasicPageLayout = ({ children, title }) => {
+  const { instance: fhevmInstance } = useFhevm();
+  const [balance, setBalance] = useState(null);
+  const [balanceLoading, setBalanceLoading] = useState(false);
+  const [usdcBalanceLoading, setUsdcBalanceLoading] = useState(false);
+  const [usdcBalance, setUsdcBalance] = useState(null);
   const pathname = usePathname();
   const [mintLoading, setMintLoading] = useState(false);
   const [claimLoading, setClaimLoading] = useState(false);
@@ -60,11 +66,166 @@ const BasicPageLayout = ({ children, title }) => {
     }
   };
 
-  const claimTokens = async () => {};
+  const getUSDCBalance = async () => {
+    try {
+      setUsdcBalanceLoading(true);
+      const erc20Contract = new Contract(
+        ERC20CONTRACTADDRESS,
+        ERC20CONTRACTABI,
+        signer
+      );
+      const balance = await erc20Contract.balanceOf(address);
+      console.log(balance.toString());
+      setUsdcBalance(balance.toString());
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setUsdcBalanceLoading(false);
+    }
+  };
+
+  const getBalance = async () => {
+    setBalanceLoading(true);
+    // setError(null);
+    try {
+      const { publicKey, privateKey } = fhevmInstance.generateKeypair();
+      const eip712 = fhevmInstance.createEIP712(
+        publicKey,
+        ENCRYPTEDERC20CONTRACTADDRESS
+      );
+
+      const signature = await signer._signTypedData(
+        eip712.domain,
+        { Reencrypt: eip712.types.Reencrypt },
+        eip712.message
+      );
+      const encryptedErc20Contract = new Contract(
+        ENCRYPTEDERC20CONTRACTADDRESS,
+        ENCRYPTEDERC20CONTRACTABI,
+        signer
+      );
+      const balanceHandle = await encryptedErc20Contract.balanceOf(address);
+      console.log("balanceHandle", balanceHandle);
+      if (balanceHandle.toString() === "0") {
+        console.log("You have no balance to fetch");
+        // toast.error("You have no balance to fetch.");
+        setBalance(null);
+      } else {
+        const balanceResult = await fhevmInstance.reencrypt(
+          balanceHandle,
+          privateKey,
+          publicKey,
+          signature.replace("0x", ""),
+          ENCRYPTEDERC20CONTRACTADDRESS,
+          address
+        );
+        console.log(balanceResult);
+        setBalance(balanceResult.toString());
+      }
+    } catch (err) {
+      console.error(err);
+      setBalance(null);
+      // toast.error("Failed to fetch balance. Please try again.");
+      // setError("Failed to fetch balance. Please try again.");
+      // setIsErrorModalOpen(true);
+    } finally {
+      console.log("completed");
+      setBalanceLoading(false);
+    }
+  };
+
+  const claimTokens = async () => {
+    try {
+      setClaimLoading(true);
+      const encryptedErc20Contract = new Contract(
+        ENCRYPTEDERC20CONTRACTADDRESS,
+        ENCRYPTEDERC20CONTRACTABI,
+        signer
+      );
+
+      const erc20Contract = new Contract(
+        ERC20CONTRACTADDRESS,
+        ERC20CONTRACTABI,
+        signer
+      );
+
+      const response = await encryptedErc20Contract.claim({
+        gasLimit: 1000000,
+      });
+      const tx = await response.getTransaction();
+      console.log(tx);
+      toast.success(
+        "Transaction sent successfully, waiting for confirmation..."
+      );
+
+      let count = 1;
+      if (count === 1) {
+        erc20Contract.on("Transfer", (from, to, value) => {
+          if (count === 1) {
+            console.log(from, to, value);
+            toast.success("Tokens claimed successfully");
+            count++;
+          }
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setClaimLoading(false);
+    }
+  };
   return (
     <>
       <div className="py-8 text-2xl font-semibold max-w-4xl mx-auto flex items-center justify-between">
-        {title}
+        {pathname === "/transfer" ? (
+          <div className="flex gap-2 items-center">
+            <Button
+              className="flex gap-2"
+              variant="outline"
+              onClick={getUSDCBalance}
+              disabled={usdcBalanceLoading}
+            >
+              {usdcBalanceLoading ? (
+                <Loader className="animate-spin" />
+              ) : (
+                <>
+                  {usdcBalance ? (
+                    `USDC Balance: ${usdcBalance}`
+                  ) : (
+                    <>
+                      <RefreshCcw size={16} />
+                      USDC
+                    </>
+                  )}
+                </>
+              )}
+            </Button>
+            <Button
+              className="flex gap-2"
+              variant="outline"
+              onClick={getBalance}
+              disabled={balanceLoading}
+            >
+              {balanceLoading ? (
+                <Loader className="animate-spin" />
+              ) : (
+                <>
+                  {balance ? (
+                    `cUSDC Balance: ${balance}`
+                  ) : (
+                    <>
+                      <RefreshCcw size={16} />
+                      CUSDC
+                    </>
+                  )}
+                </>
+              )}
+              {/* nce */}
+            </Button>
+          </div>
+        ) : (
+          title
+        )}
         {pathname === "/transfer" && (
           <div className="flex items-center gap-2">
             <Button onClick={mintTokens} disabled={mintLoading} variant="ghost">
